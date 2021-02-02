d9c8d24caba4be6557b2fc4b377a54ed55b6f1cb
남경호 <namgoo@namgyeonghoui-iMac.local>
2021-02-02 22:46:00 +0900

    처음 저장

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb+srv://namgoo:abcd1234@boilerplate.7tlvd.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
