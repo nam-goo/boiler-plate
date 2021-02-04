8f5df7f546e50cadc28764683a88a8cb959f4646
남경호 <namgoo@namgyeonghoui-iMac.local>
2021-02-04 23:07:35 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+console.log(config);
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extend: true }));
+
+//application/json
+app.use(bodyParser.json());
 const mongoose = require('mongoose');
 //몽구스
 mongoose
-  .connect(
-    'mongodb+srv://namgoo:abcd1234@boilerplate.7tlvd.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world'));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ succenss: false, err });
+    }
+    return res.status(200).json({ succenss: true });
+  });
+});
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
